df0adda09299e01948ee2e1339926495678d77cd
Maxime Challon <maxime.challon@gmail.com>
2020-07-04 09:58:24 +0200

    ajout de l'import du package

--- AdresseParser/__init__.py
+++ AdresseParser/__init__.py
@@ -1,2 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Ce module permet d'extraire d'une chaîne de caractères une adresse française et de retourner un dictionnaire
+    avec numéro de rue, rue, ville, code postal, département, région et pays.
+"""
+
+from .AdresseParser import AdresseParser
+
 __version__="0.0.1"
 __title__="AddresseParser"
